959d0392c8ff065d4dc0e28d357a74f53b746fdd
Joaquin Popoca <joaquin.popoca@wizeline.com>
2024-02-29 12:27:25 -0600

    feat: add redis connection status decorator
    To handle client connection error when Redis server is down a decaorator was introduces to throw an error in the redis handler.

--- src/infraestructure/redis/index.ts
+++ src/infraestructure/redis/index.ts
@@ -1,16 +1,5 @@
-import { createClient } from 'redis';
 import QuizDB from './QuizDB.js';
 import { redisConfig } from '../../config.js';
-
 const { user, password, host, port } = redisConfig;
 
-const redisClient = createClient({
-    url: `redis://${user}:${password}@${host}:${port}`,
-});
-
-redisClient.on('error', err => console.log('Redis Client Error', err));
-
-await redisClient.connect();
-
-
-export const quizDB = QuizDB({ client: redisClient });
+export const quizDB = QuizDB({ url: `redis://${user}:${password}@${host}:${port}` });
